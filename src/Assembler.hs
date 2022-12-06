6e70b2095aab9f4969c4f4bd4739915487e7ab81
S Takumi <takumi.shiratori@gmail.com>
2022-12-07 00:47:37 +0900

    implement assembler with symbol

--- src/Assembler.hs
+++ src/Assembler.hs
@@ -2,6 +2,7 @@ module Assembler (
 assembler ) where
 
 import Data.Char (isDigit, isSpace)
+import Data.List (mapAccumL)
 import qualified Data.Bits as Bits
 import qualified Data.Map as Map
 
@@ -10,13 +11,16 @@ type AsmCode = String
 data Command    = ACommand {value :: Value} 
                 | CCommand {dest :: Dest, comp :: Comp, jump :: Jump}
                 | LCommand {label :: Symbol}
-                deriving Show
+                deriving (Show, Eq)
 
-data Value = Imd Int | Sym Symbol deriving Show
+data Value = Imd Int | Sym Symbol deriving (Show, Eq)
 type Dest = String
 type Comp = String
 type Jump = String
 type Symbol = String
+
+type Address = Int
+type SymbolTable = (Map.Map Symbol Address, Address)
 
 codeWidth :: Int
 codeWidth = 16
@@ -33,8 +37,8 @@ instance Show Code where
             bitChar True = '1'
             bitChar False = '0'
 
-testAsm :: [String]
-testAsm = 
+testAsm1 :: [String]
+testAsm1 = 
     [ "// This file is part of www.nand2tetris.org"
     , "// and the book \"The Elements of Computing Systems\""
     , "// by Nisan and Schocken, MIT Press."
@@ -49,6 +53,34 @@ testAsm =
     , "@0"
     , "M=D"]
 
+testAsm2 :: [String]
+testAsm2 = 
+    [ "// This file is part of www.nand2tetris.org"
+    , "// and the book \"The Elements of Computing Systems\""
+    , "// by Nisan and Schocken, MIT Press."
+    , "// File name: projects/06/max/Max.asm"
+    , ""
+    , "// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])"
+    , ""
+    , "   @R0"
+    , "   D=M              // D = first number"
+    , "   @R1"
+    , "   D=D-M            // D = first number - second number"
+    , "   @OUTPUT_FIRST"
+    , "   D;JGT            // if D>0 (first is greater) goto output_first"
+    , "   @R1"
+    , "   D=M              // D = second number"
+    , "   @OUTPUT_D"
+    , "   0;JMP            // goto output_d"
+    , "(OUTPUT_FIRST)"
+    , "   @R0             "
+    , "   D=M              // D = first number"
+    , "(OUTPUT_D)"
+    , "   @R2"
+    , "   M=D              // M[2] = D (greatest number)"
+    , "(INFINITE_LOOP)"
+    , "   @INFINITE_LOOP"
+    , "   0;JMP            // infinite loop"]
 -- Remove white space & comments
 arrangeCode :: [AsmCode] -> [AsmCode]
 arrangeCode = filter (not . null) . map arrangeALine
@@ -62,7 +94,31 @@ arrangeALine (c:cs)
 
 -- Assembler
 assembler :: [AsmCode] -> [String]
-assembler = map (show . code . parseCommand) . arrangeCode
+--assembler = map (show . code . parseCommand) . arrangeCode
+assembler acs = map show cds
+    where
+        cs1 = map parseCommand . arrangeCode $ acs
+        t = symbolTable cs1
+        cs2 = filter (not . isLCommand) cs1
+        (_, cds) = mapAccumL code t cs2
+
+symbolTable :: [Command] -> SymbolTable
+symbolTable cs = foldl (\t (a, LCommand s) -> addRomAddr s a t) initSymbolTable romAddr
+    where
+        romAddr = filter (isLCommand . snd) $ romAddress cs
+
+isLCommand :: Command -> Bool
+isLCommand (LCommand _) = True
+isLCommand _ = False
+
+romAddress :: [Command] ->[(Address, Command)]
+romAddress cs = zip addrs cs
+    where
+        f ([], _) (LCommand _)  = ([0], 0)
+        f ([], _) _             = ([0], 1)
+        f (xs, next) (LCommand _)   = (next:xs, next)
+        f (xs, next) _              = (next:xs, next + 1)
+        addrs = reverse . fst $ foldl f ([], 0) cs
 
 -- parser
 splitString :: Char -> String -> (String, String)
@@ -101,15 +157,20 @@ parseCommand s = CCommand {dest = s1, comp = s2, jump = s3}
         (sc', sj') = splitString ';' s    
 
 
-code :: Command -> Code
-code c = case c of
-    ACommand v     -> codeACommand v
-    CCommand d c j -> codeCCommand d c j
-    LCommand s     -> codeLCommand s
+code :: SymbolTable -> Command ->  (SymbolTable, Code)
+code t c = case c of
+    ACommand v     -> codeACommand t v
+    CCommand d c j -> (t, codeCCommand d c j)
+    LCommand s     -> error "code: can not make code"
 
-codeACommand :: Value -> Code
-codeACommand (Imd n) = Code n
-codeACommand (Sym s) = error "not implemented"
+codeACommand :: SymbolTable -> Value -> (SymbolTable, Code)
+codeACommand t (Imd n) = (t, Code n)
+codeACommand t (Sym s) = (t', Code n')
+    where
+        (t', n')
+            | contains s t = case getAddress s t of
+                a -> (t, a)
+            | otherwise = addRamAddr s t
 
 codeCCommand :: Dest -> Comp -> Jump -> Code
 codeCCommand d c j = Code n
@@ -184,7 +245,26 @@ codeJump j = m Map.! j
             , ("JMP", 7)]
 
 
-codeLCommand :: Symbol -> Code
-codeLCommand = error "not implemented"
+initSymbolTable :: SymbolTable
+initSymbolTable = (Map.fromList $ t1 ++ t2 ++ t3, 16)  
+    where
+        t1 = [ ("SP", 0x0000)
+             , ("LCL", 0x0001)
+             , ("ARG", 0x0002)
+             , ("THIS", 0x0003)
+             , ("THAT", 0x0004)]
+        t2 = [ ("R" ++ show i, i) | i <- [0..15] ]
+        t3 = [ ("SCREEN", 0x4000)
+             , ("KBD", 0x6000)]
 
+addRomAddr :: Symbol -> Address -> SymbolTable -> SymbolTable
+addRomAddr s a (m, aNext) = (Map.insert s a m, aNext)
 
+addRamAddr :: Symbol -> SymbolTable -> (SymbolTable, Address)
+addRamAddr s (m, aNext) = ((Map.insert s aNext m, aNext + 1), aNext)
+
+contains :: Symbol -> SymbolTable -> Bool
+contains s (m, _) = Map.member s m
+
+getAddress :: Symbol -> SymbolTable -> Address
+getAddress s (m, _) = m Map.! s
